489650cd7ac060519a28306bc8ddb568f18af953
Ton That Minh Nhat <e0224917@u.nus.edu>
2017-10-06 21:03:01 +0800

    Create DTOs + some commands for DAO

---
+++ SA45TEAM07_VEHICLE/Bus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA45TEAM07_VEHICLE
+{
+    class Bus: Vehicle
+    {
+        int maxPassenger;
+        int tvQty;
+
+        public int MaxPassenger { get => maxPassenger; set => maxPassenger = value; }
+        public int TvQty { get => tvQty; set => tvQty = value; }
+    }
+}
