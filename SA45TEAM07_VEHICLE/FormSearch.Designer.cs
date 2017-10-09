7e44d8bc4f4480fa29efc6d6b7d06526b976bc18
CK Tan <tanck105@hotmail.com>
2017-10-09 21:37:18 +0800

    Minor change

--- SA45TEAM07_VEHICLE/FormSearch.Designer.cs
+++ SA45TEAM07_VEHICLE/FormSearch.Designer.cs
@@ -39,10 +39,9 @@
             // btnSearch
             // 
             this.btnSearch.Enabled = false;
-            this.btnSearch.Location = new System.Drawing.Point(118, 143);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Location = new System.Drawing.Point(139, 93);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(159, 48);
+            this.btnSearch.Size = new System.Drawing.Size(106, 31);
             this.btnSearch.TabIndex = 2;
             this.btnSearch.Text = "Submit";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -51,10 +50,9 @@
             // btnClose
             // 
             this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnClose.Location = new System.Drawing.Point(372, 143);
-            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Location = new System.Drawing.Point(282, 93);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(159, 48);
+            this.btnClose.Size = new System.Drawing.Size(106, 31);
             this.btnClose.TabIndex = 3;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
@@ -66,11 +64,9 @@
             this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.txbPlateNum);
-            this.groupBox1.Location = new System.Drawing.Point(39, 21);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Location = new System.Drawing.Point(26, 19);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.groupBox1.Size = new System.Drawing.Size(650, 239);
+            this.groupBox1.Size = new System.Drawing.Size(433, 156);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             // 
@@ -78,33 +74,31 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 54);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(13, 35);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(149, 23);
+            this.label1.Size = new System.Drawing.Size(94, 15);
             this.label1.TabIndex = 1;
             this.label1.Text = "Plate Number:";
             // 
             // txbPlateNum
             // 
             this.txbPlateNum.Font = new System.Drawing.Font("Lucida Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbPlateNum.Location = new System.Drawing.Point(208, 43);
-            this.txbPlateNum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbPlateNum.Location = new System.Drawing.Point(139, 28);
             this.txbPlateNum.Name = "txbPlateNum";
-            this.txbPlateNum.Size = new System.Drawing.Size(410, 30);
+            this.txbPlateNum.Size = new System.Drawing.Size(249, 23);
             this.txbPlateNum.TabIndex = 0;
             this.txbPlateNum.TextChanged += new System.EventHandler(this.txbPlateNum_TextChanged);
             // 
             // FormSearch
             // 
             this.AcceptButton = this.btnSearch;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnClose;
-            this.ClientSize = new System.Drawing.Size(729, 302);
+            this.ClientSize = new System.Drawing.Size(486, 197);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 8, 4, 8);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FormSearch";
--- SA45TEAM07_VEHICLE/FormSearch.cs
+++ SA45TEAM07_VEHICLE/FormSearch.cs
@@ -51,27 +51,22 @@ namespace SA45TEAM07_VEHICLE
             enquiryControl.CloseUseCase();
         }
 
-        private void FormSearch_LocationChanged(object sender, EventArgs e)
-        {
-            this.Location = this.Location;
-        }
+        //protected override void WndProc(ref Message message)
+        //{
+        //    const int WM_SYSCOMMAND = 0x0112;
+        //    const int SC_MOVE = 0xF010;
 
-        protected override void WndProc(ref Message message)
-        {
-            const int WM_SYSCOMMAND = 0x0112;
-            const int SC_MOVE = 0xF010;
+        //    switch (message.Msg)
+        //    {
+        //        case WM_SYSCOMMAND:
+        //            int command = message.WParam.ToInt32() & 0xfff0;
+        //            if (command == SC_MOVE)
+        //                return;
+        //            break;
+        //    }
 
-            switch (message.Msg)
-            {
-                case WM_SYSCOMMAND:
-                    int command = message.WParam.ToInt32() & 0xfff0;
-                    if (command == SC_MOVE)
-                        return;
-                    break;
-            }
-
-            base.WndProc(ref message);
-        }
+        //    base.WndProc(ref message);
+        //}
 
         private void txbPlateNum_TextChanged(object sender, EventArgs e)
         {
