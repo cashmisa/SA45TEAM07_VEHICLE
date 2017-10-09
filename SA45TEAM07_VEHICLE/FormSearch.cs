593eeed935f7a3ac860f448425da3d54e6fe7717
MengGuan <MengGuan@MENGGUAN>
2017-10-09 10:29:40 +0800

    make formsearch form unmovable as it will be covered by teal text

--- SA45TEAM07_VEHICLE/FormSearch.cs
+++ SA45TEAM07_VEHICLE/FormSearch.cs
@@ -46,5 +46,26 @@ namespace SA45TEAM07_VEHICLE
             enquiryControl.closeUseCase();
         }
 
+        private void FormSearch_LocationChanged(object sender, EventArgs e)
+        {
+            this.Location = this.Location;
+        }
+
+        protected override void WndProc(ref Message message)
+        {
+            const int WM_SYSCOMMAND = 0x0112;
+            const int SC_MOVE = 0xF010;
+
+            switch (message.Msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = message.WParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                        return;
+                    break;
+            }
+
+            base.WndProc(ref message);
+        }
     }
 }
